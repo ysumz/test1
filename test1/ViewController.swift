1da207189092a460b23e4577e4347727a760b480
ysumz <locoyyjr@gmail.com>
2018-10-28 15:58:21 +0900

    1行目 : コミットでの変更内容の要約 2行目 : 空行 3行目以降 : 変更した理由

---
+++ test1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test1
+//
+//  Created by 清水悠平 on 2018/10/28.
+//  Copyright © 2018 清水悠平. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ test1UITests/test1UITests.swift
@@ -0,0 +1,34 @@
+//
+//  test1UITests.swift
+//  test1UITests
+//
+//  Created by 清水悠平 on 2018/10/28.
+//  Copyright © 2018 清水悠平. All rights reserved.
+//
+
+import XCTest
+
+class test1UITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
